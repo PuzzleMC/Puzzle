f28f2cd38b1a13271229924643d8d93de23bfc03
Motschen <motschenfm@gmail.com>
2021-06-09 09:32:21 +0200

    Change the update URL to match new organization

--- puzzle-base/src/main/java/net/puzzlemc/core/PuzzleCore.java
+++ puzzle-base/src/main/java/net/puzzlemc/core/PuzzleCore.java
@@ -9,10 +9,10 @@ public class PuzzleCore implements ClientModInitializer {
     public final static String version = "Puzzle A1";
     public final static String name = "Puzzle";
     public final static String id = "puzzle";
-    public final static String website = "https://github.com/TeamMidnightDust/Puzzle";
+    public final static String website = "https://github.com/PuzzleMC/Puzzle";
     public static String updateURL = website; //+"download";
 
-    public final static String UPDATE_URL = "https://raw.githubusercontent.com/TeamMidnightDust/Puzzle/1.17/puzzle_versions.json";
+    public final static String UPDATE_URL = "https://raw.githubusercontent.com/PuzzleMC/Puzzle/1.17/puzzle_versions.json";
 
     @Override
     public void onInitializeClient() {
