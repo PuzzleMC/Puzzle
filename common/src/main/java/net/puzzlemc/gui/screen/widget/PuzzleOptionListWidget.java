2ddfe4bd25bb97990cd689094c7dbdd9265783fe
Martin Prokoph <motschen@midnightdust.eu>
2025-07-15 17:34:49 +0200

    hotfix: MidnightLib 1.7.5 compat
    - Also fixes crashes when using the EMF mod

--- common/src/main/java/net/puzzlemc/gui/screen/widget/PuzzleOptionListWidget.java
+++ common/src/main/java/net/puzzlemc/gui/screen/widget/PuzzleOptionListWidget.java
@@ -13,6 +13,7 @@ import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.Formatting;
 import net.puzzlemc.gui.screen.PuzzleOptionsScreen;
 
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class PuzzleOptionListWidget extends MidnightConfig.MidnightConfigListWid
         int buttonX = this.width - 160;
         for (PuzzleWidget button : buttons) {
             if (button.buttonType == ButtonType.TEXT)
-                this.addButton(List.of(), Text.literal(" ").append(button.descriptionText).formatted(Formatting.BOLD));
+                this.addButton(List.of(), Text.literal("").append(button.descriptionText).formatted(Formatting.BOLD));
             else if (button.buttonType == ButtonType.BUTTON)
                 this.addButton(List.of(new PuzzleButtonWidget(buttonX, 0, 150, 20, button.buttonTextAction, button.onPress)), button.descriptionText);
             else if (button.buttonType == ButtonType.SLIDER)
@@ -44,8 +45,19 @@ public class PuzzleOptionListWidget extends MidnightConfig.MidnightConfigListWid
         }
     }
     public void addButton(List<ClickableWidget> buttons, Text text) {
-        var entry = new MidnightConfig.ButtonEntry(buttons, text, new MidnightConfig.EntryInfo());
-        entry.centered = buttons.isEmpty();
+        var info = new MidnightConfig.EntryInfo(null, "puzzle-gui");
+        info.comment = new MidnightConfig.Comment() {
+            @Override
+            public boolean centered() {
+                return buttons.isEmpty();
+            }
+            public Class<? extends Annotation> annotationType() {return null;}
+            public String category() {return "";}
+            public String name() {return "";}
+            public String url() {return "";}
+            public String requiredMod() {return "";}
+        };
+        var entry = new MidnightConfig.ButtonEntry(buttons, text, info);
         this.addEntry(entry);
     }
     public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
